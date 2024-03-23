f891e9a80de2878226ef7a7f5edc8f8863986c8f
RehamMuhammed <rehammuhammedadel@gmail.com>
2024-03-23 02:43:33 +0200

    Adding git ignore

--- src/App.js
+++ src/App.js
@@ -11,33 +11,47 @@ import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
 import { useState } from 'react';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DonationThoSoYk from './components/DonationThSoYk/DonationThSoYk';
- 
+import Toaster from 'react-hot-toast';
+
 import app from './FireBase.config';
 
 function App() {
-const [toggle ,setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
   return (
-    <Router>
-    <Navbar toggle={toggle} setToggle={setToggle}/>
-  <div style={{minHeight: "80vh"}}>
-  <Routes>
-      <Route path="/donation-form" element={<DonationThoSoYk/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/buy" element={<BuyHome/>}/>
-      <Route path="/" element={<Home/>}/>
-    </Routes>
-  </div>
-    {/* <Header/>
-    <About/> */}
-    {/* <DonationThoSoYk/> */}
-    <Footer toggle={toggle}/>
-    
-    </Router>
-   
+    <div>
+      <Router>
+        <Navbar toggle={toggle} setToggle={setToggle} />
+        <div style={{ minHeight: "80vh" }}>
+          <Routes>
+            <Route path="/donation-form" element={<DonationThoSoYk />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/buy" element={<BuyHome />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+
+        <Footer toggle={toggle} />
+
+
+      </Router>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: '',
+          duration: 5000,
+        }}
+      />
+    </div>
+
   );
 }
 
