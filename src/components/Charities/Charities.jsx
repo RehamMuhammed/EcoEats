8315904f0109414089ab17188c7b4ed1c3a58d0c
RehamMuhammed <rehammuhammedadel@gmail.com>
2024-03-23 06:35:50 +0200

    Preparing search functionality

--- src/components/Charities/Charities.jsx
+++ src/components/Charities/Charities.jsx
@@ -3,22 +3,39 @@ import CharityCard from './CharityCard';
 
 function Charities() {
   const [charities, setCharities] = useState([])
-  const fetchData = useCallback( async() => {
+  const [filter, setFilter] = useState([])
+  const [searchValue, setSearchValue] = useState("")
+  const fetchData = useCallback(async () => {
     const res = await fetch("https://ecoeatsapp.wiremockapi.cloud/charities/get")
-    const {result} = await res.json()
+    const { result } = await res.json()
     console.log(result)
     setCharities(result)
-  },[]) 
+  }, [])
   useEffect(() => {
-    fetchData(  )
-    },[])
+    fetchData()
+  }, [])
+  const handleSearch = (e) => {
+    setSearchValue(e.target.value)
+  }
+  useEffect(() => {
+    const filterCharities = charities.filter(charity => {
+      console.log(charity.CharityName.includes(searchValue))
+      console.log(charity.CharityName, searchValue)
+      return (charity.CharityName.includes(searchValue))
+    })
+    console.log(filterCharities)
+    setFilter(filterCharities) 
+  }, [searchValue])
+
   return (
     <div className='container3'>
-    <div className='d-flex flex-wrap py-5 gap-5 justify-content-center' >
-      {
-        charities.map(charity => <CharityCard key={charity.Hotline} charity={charity}/> )
-      }
-    </div>
+      {/* <input value={searchValue} class="form-control me-2" type="search" placeholder="Search" aria-label="Search" onChange={handleSearch} /> */}
+
+      <div className='d-flex flex-wrap py-5 gap-5 justify-content-center' >
+        {
+          (searchValue === "" ? charities : filter).map(charity => <CharityCard key={charity.Hotline} charity={charity} />)
+        } 
+      </div>
     </div>
   );
 }
