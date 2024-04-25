2b0093d7bad00f61d0c9aaefaee9e62bdba6e986
HabibaAhmedd <habibaahmed19372456@gmail.com>
2024-04-25 20:11:51 +0200

    buy home and profile css changes

--- src/components/Buy/BuyHome.jsx
+++ src/components/Buy/BuyHome.jsx
@@ -5,11 +5,11 @@ import { FaPaperPlane } from "react-icons/fa";
 const BuyHome = () => {
   return (
     <header className='buy flex flex-center flex-column'>
-      <div className='container-buy'>
-        <div className='buy-content text-center flex flex-column'>
+      <div className='container-buy text-center'>
+        <div className='buy-content text-center flex flex-column align-items-center'>
           <h1 className='text-uppercase buy-title'>Welcome to EcoEats</h1>
-          <p className='text-lead'>Here, you can buy any food with very good offers. Welcome to our website and we're waiting for your donations to the charities.</p>
-
+          <br></br>
+          <p className='text-lead'>Here, you can buy any food with very good offers. Welcome to our website and we're waiting for your donations to the charities.</p><br></br><br></br><br></br><br></br>
           <a href="store" className='btn buy-btn'>
 
 
