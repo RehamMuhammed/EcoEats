28dadf66c21b4fd6a2e7ffdea46ee1053bdbf7e7
RehamMuhammed <rehammuhammedadel@gmail.com>
2024-06-18 22:39:55 +0300

    Thank you Page

--- src/components/CharityList/DonationDone.jsx
+++ src/components/CharityList/DonationDone.jsx
@@ -3,9 +3,22 @@ import Styles from './DonationDone.css';
 import { Link } from 'react-router-dom';
 
 function DonationDone(props) {
-    return <div className='d-flex flex-column align-items-center'>
-        <h1 className='text-dark'>Thank you! 🎉</h1>
-        <Link to='/' className='done-btn'>Back to Home Page</Link>
+    // return <div className='d-flex flex-column align-items-center'>
+    //     <h1 className='text-dark'>Thank you! 🎉</h1>
+    //     <Link to='/' className='done-btn'>Back to Home Page</Link>
+    // </div>
+    return <div class="content">
+        <div class="wrapper-1">
+            <div class="wrapper-2">
+                <h1>Thank you !</h1>
+                <p>Thank you for your generous Donation!  </p>
+                <p>you should receive a confirmation email soon  </p>
+                <Link to='/' class="go-home">
+                    Back to Home
+
+                </Link>
+            </div>
+        </div>
     </div>
 }
 
--- src/components/Payment/Completion.jsx
+++ src/components/Payment/Completion.jsx
@@ -3,9 +3,21 @@ import Styles from './Checkout.module.css';
 import { Link } from 'react-router-dom';
 
 function Completion(props) {
-    return <div className='d-flex flex-column align-items-center'>
-        <h1 className='text-dark'>Thank you! 🎉</h1>;
-        <Link to='/buy' className={`${Styles.pay_btn}`}>Back to Home Page</Link>
+    // return <div className='d-flex flex-column align-items-center'>
+    //     <h1 className='text-dark'>Thank you! 🎉</h1>;
+    //     <Link to='/buy' className={`${Styles.pay_btn}`}>Back to Home Page</Link>
+    // </div>
+    return <div class="contentt">
+        <div class="wrapper-11">
+            <div class="wrapper-22">
+                <h1>Thank you !</h1>
+                <p>Thank you for your generous Donation!  </p>
+                <p>you should receive a confirmation email soon  </p>
+                <Link to='/buy' className={`${Styles.go_home1}`}>
+                    Back to Home
+                </Link>
+            </div>
+        </div>
     </div>
 }
 
